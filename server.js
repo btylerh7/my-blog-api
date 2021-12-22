69e3433978f1e6d1611a2380fc09b8809e087d96
robustjumprope <91493312+robustjumprope@users.noreply.github.com>
2021-12-22 11:33:39 -0600

    Set up basic server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+//configure express app and connect to mongodb
+dotenv.config()
+const app = express()
+const myDb = process.env.MONGO_URI
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+mongoose.connect(myDb,{ useNewUrlParser: true, useUnifiedTopology: true })
+
+
+//Get routes
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+//Post routes
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
